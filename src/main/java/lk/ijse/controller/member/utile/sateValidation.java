b88bc16381045cb4589174eca54d7efa5b7d2e1d
ApsaraWitharana <asachini095@gmail.com>
2024-03-11 22:44:52 +0530

    validate library

--- src/main/java/lk/ijse/controller/member/utile/sateValidation.java
+++ src/main/java/lk/ijse/controller/member/utile/sateValidation.java
@@ -29,6 +29,7 @@ public class sateValidation {
 
     }
     public static boolean bookIdCheck(String book_id) {
+
         String pattern = "^B-\\d+$";  //B-001
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(book_id);
@@ -37,9 +38,19 @@ public class sateValidation {
     }
 
     public static boolean libraryIdCheck(String library_id) {
+
         String pattern = "^L-\\d+$";  //L-001
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(library_id);
+        return m.matches();
+
+    }
+
+    public static boolean contactNumberCheck(String contact ) {
+
+        String pattern = "'^(\\+94|0)?(7\\d{2}|[1-6]\\d{1}|8[0-1])\\d{7}$";  //071-2345678
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(contact);
         return m.matches();
 
     }
