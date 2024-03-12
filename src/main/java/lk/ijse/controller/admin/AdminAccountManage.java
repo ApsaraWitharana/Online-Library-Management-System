65d22c49ba7e75746891cd768d02da8ee9836e10
ApsaraWitharana <asachini095@gmail.com>
2024-03-12 22:08:51 +0530

    initialized admin profile pic

--- src/main/java/lk/ijse/controller/admin/AdminAccountManage.java
+++ src/main/java/lk/ijse/controller/admin/AdminAccountManage.java
@@ -2,8 +2,20 @@ package lk.ijse.controller.admin;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class AdminAccountManage {
 
@@ -13,9 +25,35 @@ public class AdminAccountManage {
     @FXML
     private TextField txtAdminName;
 
+    public static Image image;
+
     @FXML
     void adminNameOnAction(ActionEvent event) {
 
     }
+
+    @FXML
+    void setProfileOnAction(MouseEvent actionEvent) {
+
+        Window window = ((Node) (actionEvent.getSource())).getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(window);
+        actionEvent.consume();
+        try {
+            InputStream in = new FileInputStream(file);
+            image = new Image(in);
+            circle.setFill(new ImagePattern(image));
+
+        } catch (FileNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+        if(image==null) {
+            image = new Image("/image/images-wp-profile.jpeg");
+        }
+        circle.setFill(new ImagePattern(image));
+    }
+
+
+}
 
 }
