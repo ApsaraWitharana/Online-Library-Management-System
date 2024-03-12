4e505823fd228b4a3f849e0387151e3d6f74861e
ApsaraWitharana <asachini095@gmail.com>
2024-03-12 12:01:19 +0530

    create BOFactory and DAOFactory

--- src/main/java/lk/ijse/bo/BOFactory.java
+++ src/main/java/lk/ijse/bo/BOFactory.java
@@ -1,4 +1,37 @@
 package lk.ijse.bo;
 
+import lk.ijse.bo.custom.impl.BookBOImpl;
+import lk.ijse.bo.custom.impl.IssueBookBOImpl;
+import lk.ijse.bo.custom.impl.LibraryBOImpl;
+import lk.ijse.bo.custom.impl.UserBOImpl;
+
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory(){
+
+    }
+
+    public static BOFactory getBoFactory(){
+        return (boFactory == null) ? boFactory=new BOFactory():boFactory;
+
+    }
+
+    public enum BOType{
+        BOOK,LIBRARY,USER,ISSUE_BOOK,USER_REGISTRATION
+    }
+
+    public SuperBO getBO(BOType boType){
+        switch (boType){
+
+            case BOOK :return new BookBOImpl();
+            case USER:return new UserBOImpl();
+            case LIBRARY:return new LibraryBOImpl();
+            case ISSUE_BOOK:return new IssueBookBOImpl();
+            case USER_REGISTRATION:return new UserBOImpl();
+
+            default:return null;
+        }
+    }
 }
--- src/main/java/lk/ijse/dao/DAOFactory.java
+++ src/main/java/lk/ijse/dao/DAOFactory.java
@@ -1,4 +1,38 @@
 package lk.ijse.dao;
 
+import lk.ijse.dao.custom.LibraryDAO;
+import lk.ijse.dao.custom.impl.*;
+
 public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory == null) ? daoFactory = new DAOFactory(): daoFactory;
+    }
+
+    public enum DAOTypes{
+        BOOK,USER,LIBRARY,ISSUE_BOOK,USER_REGISTRATION,SIGNUP,LOGIN,SETTING
+    }
+
+    public SuperDAO getDAO(DAOTypes daoTypes){
+        switch (daoTypes){
+            case USER : return new UserDAOImpl();
+            case USER_REGISTRATION:return new UserDAOImpl();
+            case ISSUE_BOOK:return new IssueBookDAOImpl();
+            case LIBRARY:return new LibraryDAOImpl();
+            case BOOK: return new BookDAOImpl();
+            case SIGNUP:return new SignupDAOImpl();
+            case LOGIN:return new LoginDAOImpl();
+            case SETTING:return new LoginDAOImpl();
+
+            default:return null;
+
+
+        }
+    }
 }
