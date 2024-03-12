ed1cc225bdecc1fbabddf8411674d6e995f7cf33
ApsaraWitharana <asachini095@gmail.com>
2024-03-12 22:02:07 +0530

    create admin account manage form

---
+++ src/main/java/lk/ijse/controller/admin/AdminAccountManage.java
@@ -0,0 +1,21 @@
+package lk.ijse.controller.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+public class AdminAccountManage {
+
+    @FXML
+    private Circle circle;
+
+    @FXML
+    private TextField txtAdminName;
+
+    @FXML
+    void adminNameOnAction(ActionEvent event) {
+
+    }
+
+}
