3d83a39450eda7fee78651df5c30741457ea114f
ApsaraWitharana <asachini095@gmail.com>
2024-03-12 13:45:31 +0530

    create user setting change user name and password

--- src/main/java/lk/ijse/controller/member/SettingFormController.java
+++ src/main/java/lk/ijse/controller/member/SettingFormController.java
@@ -1,9 +1,11 @@
 package lk.ijse.controller.member;
 
+import com.ctc.wstx.shaded.msv_core.grammar.util.NameClassComparator;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
@@ -13,8 +15,10 @@ import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import lk.ijse.bo.custom.SettingBO;
 import lk.ijse.bo.custom.impl.SettingBOImpl;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,6 +27,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import static lk.ijse.controller.member.LoginFormController.user;
+import static org.mindrot.jbcrypt.BCrypt.*;
 
 public class SettingFormController implements Initializable {
 
@@ -30,16 +35,26 @@ public class SettingFormController implements Initializable {
     private Circle circle;
 
     @FXML
-    private TextField txtCurrentP;
+    private PasswordField txtCurrPass;
+
+    @FXML
+    private TextField txtCurrentPass;
 
     @FXML
     private TextField txtName;
 
     @FXML
-    private TextField txtNewP;
+    private TextField txtNewPassword;
 
     @FXML
-    private TextField txtReEnPa;
+    private PasswordField txtPass;
+
+    @FXML
+    private PasswordField txtReEnter;
+
+    @FXML
+    private TextField txtconfirmValid;
+
     public static Image image;
     @FXML
     private Label lblName;
@@ -60,7 +75,57 @@ public class SettingFormController implements Initializable {
     }
     @FXML
     void PasswordOnAction(MouseEvent event) {
+        if(txtCurrPass.isVisible()) {
+            txtCurrPass.setVisible(false);
+            txtCurrentPass.setVisible(true);
+            txtCurrentPass.setText(txtCurrPass.getText());
+            txtCurrentPass.setEditable(false);
+        }else{
+            txtCurrPass.setVisible(true);
+            txtCurrentPass.setVisible(false);
+        }
+        if(txtPass.isVisible()) {
+            txtPass.setVisible(false);
+            txtNewPassword.setVisible(true);
+            txtNewPassword.setText(txtPass.getText());
+            txtNewPassword.setEditable(false);
+        }else{
+            txtPass.setVisible(true);
+            txtNewPassword.setVisible(false);
+        }
+        if(txtReEnter.isVisible()) {
+            txtReEnter.setVisible(false);
+            txtconfirmValid.setVisible(true);
+            txtconfirmValid.setText(txtReEnter.getText());
+            txtconfirmValid.setEditable(false);
+        }else{
+            txtReEnter.setVisible(true);
+            txtconfirmValid.setVisible(false);
+        }
 
+    }
+
+    public void passwordOnAction(javafx.event.ActionEvent event) {
+        System.out.println(txtCurrPass.getText());
+        System.out.println(user.getPassword());
+        if (!txtCurrPass.getText().isEmpty() && !txtPass.getText().isEmpty() && !txtReEnter.getText().isEmpty()){
+            if (BCrypt.checkpw(txtCurrPass.getText(),user.getPassword())){
+                if (txtPass.getText().equals(txtReEnter.getText())){
+                    String hashed = BCrypt.hashpw(txtReEnter.getText(),BCrypt.gensalt());
+                    boolean isUpdate = settingBO.updatePassword(hashed,user.getName());
+                    if (isUpdate){
+                        new Alert(Alert.AlertType.CONFIRMATION,"Update password successfully!!").show();
+
+                    }else
+                        new Alert(Alert.AlertType.ERROR,"Update password failed!!").show();
+
+                }else
+                    new Alert(Alert.AlertType.ERROR,"password doesn't match ! please re-enter").show();
+            }else
+                new Alert(Alert.AlertType.ERROR,"Current Password is incorrect").show();
+
+        }else
+            new Alert(Alert.AlertType.ERROR,"Please fill up all fields!").show();
     }
 
     @FXML
@@ -96,6 +161,7 @@ public class SettingFormController implements Initializable {
         }
         circle.setFill(new ImagePattern(image));
     }
+
 
 
 }
